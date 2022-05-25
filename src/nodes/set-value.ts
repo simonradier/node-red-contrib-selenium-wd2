2eb9a7d6c6a41591e09a0dc981d7a3983e2e065b
Denis Bordea <bordea.denis@gmail.com>
2022-05-25 13:33:51 +0300

    Make fill works as we want

--- src/nodes/set-value.ts
+++ src/nodes/set-value.ts
@@ -1,10 +1,11 @@
 import { WD2Manager } from "../wd2-manager";
 import { SeleniumAction, SeleniumNode, SeleniumNodeDef } from "./node";
 import { GenericSeleniumConstructor } from "./node-constructor";
+let utils = require('util');
 
 // tslint:disable-next-line: no-empty-interface
 export interface NodeSetValueDef extends SeleniumNodeDef {
-    value : string;
+    value: string;
 }
 
 // tslint:disable-next-line: no-empty-interface
@@ -12,25 +13,37 @@ export interface NodeSetValue extends SeleniumNode {
 
 }
 
-async function inputAction (node : NodeSetValue, conf : NodeSetValueDef, action : SeleniumAction) : Promise<void> {
-    return new Promise<void> (async (resolve, reject) => {
+async function inputAction(node: NodeSetValue, conf: NodeSetValueDef, action: SeleniumAction): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
         const msg = action.msg;
-        const value = msg.value ?? conf.value;
+        let value = msg.value ?? conf.value;
         try {
-			await msg.driver.executeScript("arguments[0].setAttribute('value', '" + value + "')", msg.element);
-            node.status({ fill : "green", shape : "dot", text : "success"})
+            if (conf.selector === "boundaryBox" && value === "msg.value") {
+                conf.target = Object(msg.payload)["selector"];;
+                let coordonates = conf.target.split(",");
+                let result = WD2Manager.midpoint(Number.parseInt(coordonates[0]), Number.parseInt(coordonates[1]), Number.parseInt(coordonates[2]), Number.parseInt(coordonates[3]));
+                let script = `document.elementFromPoint(${result[0]},${result[1]}).click()`;
+                await msg.driver.executeScript(script, msg.element);
+                value = Object(msg.payload)["msg"];
+                let fillScript = `document.elementFromPoint(${result[0]},${result[1]}).value = '${value}'`;
+                await msg.driver.executeScript(fillScript, msg.element);
+            }
+            else {
+                await msg.driver.executeScript("arguments[0].setAttribute('value', '" + value + "')", msg.element);
+            }
+            node.status({ fill: "green", shape: "dot", text: "success" })
             if (msg.error) { delete msg.error; }
             action.send([msg, null]);
             action.done();
-        } catch(err) {
+        } catch (err) {
             if (WD2Manager.checkIfCritical(err)) {
                 reject(err);
             } else {
                 msg.error = {
-                    message : "Can't set value on the the element : " + err.message
+                    message: "Can't set value on the the element : " + err.message
                 };
                 node.warn(msg.error.message);
-                node.status({ fill : "yellow", shape : "dot", text : "expected value error"})
+                node.status({ fill: "yellow", shape: "dot", text: "expected value error" })
                 action.send([null, msg]);
                 action.done();
             }
@@ -41,4 +54,4 @@ async function inputAction (node : NodeSetValue, conf : NodeSetValueDef, action 
 
 const NodeSetValueConstructor = GenericSeleniumConstructor(null, inputAction);
 
-export { NodeSetValueConstructor as NodeSetValueConstructor}
+export { NodeSetValueConstructor as NodeSetValueConstructor }
